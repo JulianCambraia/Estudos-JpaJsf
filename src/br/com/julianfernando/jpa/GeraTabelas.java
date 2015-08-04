4d15c2e4557d064998f026fa4f7d25e767186323
JulianFernando <juliancambraia@gmail.com>
2015-08-04 12:07:55 -0300

    atualiza provider depreciado para esta versão do Hibernate, Cria classe principal geradora de tabelas.

---
+++ src/br/com/julianfernando/jpa/GeraTabelas.java
@@ -0,0 +1,14 @@
+package br.com.julianfernando.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class GeraTabelas {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("livraria-pu");
+		factory.close();
+
+	}
+
+}
