bb1fe43a8f79f3515a68bae15bf76068107bf63b
JulianFernando <juliancambraia@gmail.com>
2015-08-04 14:33:48 -0300

    cria a classe InsereEditoraComJPA para testar a inserção via console.

---
+++ src/br/com/julianfernando/jpa/InsereEditoraComJPA.java
@@ -0,0 +1,34 @@
+package br.com.julianfernando.jpa;
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InsereEditoraComJPA {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("livraria-pu");
+		
+		EntityManager manager = factory.createEntityManager();
+		
+		Editora editora1 = new Editora();
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Informe o nome da editora:");
+		editora1.setNome(entrada.nextLine());
+		
+		System.out.println("Informe o email da editora:");
+		editora1.setEmail(entrada.nextLine());
+		
+		manager.persist(editora1);
+		
+		manager.getTransaction().begin();
+		manager.getTransaction().commit();
+		manager.close();
+		factory.close();
+	}
+
+}
